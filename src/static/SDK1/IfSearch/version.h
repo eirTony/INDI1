f7341b15d5947f38184df1844713f6769aaa7cee
eirTony <totto@eclipseir.com>
2016-11-25 23:59:59 -0800

    v2.002B - Start Qt5 static build [#3]

---
+++ src/static/SDK1/IfSearch/version.h
@@ -0,0 +1,16 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+
+#define VER_STRING     "v2.002B"
+#define VER_MAJOR      2
+#define VER_MINOR      2
+#define VER_BRANCH     0
+#define VER_RELEASE    2
+
+#define VER_FILETYPE    VFT_APP
+#define VER_APPNAME     "INDIface"
+#define VER_ICON        "INDIface24.ico"
+#define VER_FILEDESC    "EclipseIR INDIface SDK1 Console"
+
+#endif // VERSION_H

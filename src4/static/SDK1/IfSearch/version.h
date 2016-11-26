07fe9eff4fb88c6c2d636c94267ea8734d295223
eirTony <totto@eclipseir.com>
2016-11-25 23:43:06 -0800

    v1.88B - one eighty eight bee, successful build in new INDI1 github structure. [#1]

--- src4/static/SDK1/IfSearch/version.h
+++ src4/static/SDK1/IfSearch/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_STRING     "v1.88A"
+#define VER_STRING     "v1.88B"
 #define VER_MAJOR      1
 #define VER_MINOR      88
 #define VER_BRANCH     0
-#define VER_RELEASE    1
+#define VER_RELEASE    2
 
 #define VER_FILETYPE    VFT_APP
 #define VER_APPNAME     "INDIface"
